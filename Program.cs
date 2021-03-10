287d18f23f1d60bee06946b027ef1eca8d807b26
cazser <2264712072@qq.com>
2021-03-10 09:43:51 +0800

    添加教师(Teacher)类

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace courseTableGA
     {
         static void Main(string[] args)
         {
-            Room room = new Room(1, "理工一101", 120);
-            Console.WriteLine("教室{0}在{1}能容纳{2}学生", room.Id, room.Address, room.Capacity);
+            Teacher teacher = new Teacher(1, "王富贵");
+            Console.WriteLine("{0}号老师叫{1}", teacher.Id, teacher.Name);
         }
     }
 }
---
+++ teacher.cs
@@ -0,0 +1,21 @@
+public partial class Teacher
+{
+    private int teacherId;
+    private string teacherName;
+
+    public Teacher(int id, string name)
+    {
+        this.teacherId = id;
+        this.teacherName = name;
+    }
+
+    public int Id
+    {
+        get { return this.teacherId; }
+    }
+
+    public string Name
+    {
+        get { return this.teacherName; }
+    }
+}
